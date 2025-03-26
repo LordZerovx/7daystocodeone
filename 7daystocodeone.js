5b4ce40112046b0bcc11a18cd8af223241cdf01d
LordZerovx <143531437+LordZerovx@users.noreply.github.com>
2025-03-25 23:42:58 -0300

    Add files via upload

---
+++ 7daystocodeone.js
@@ -0,0 +1,36 @@
+let numeroUm = 1;
+let stringUm = '1';
+
+let numeroTrinta = 30;
+let stringTrinta = '30';
+
+let numeroDez = 10;
+let stringDez = '10';
+
+//if (numeroUm == stringUm) {
+//    console.log('as variáveis numeroUm e stringUm tem valores iguais, porém, possuem tipos diferentes!');
+////} else {
+//    console.log('As variáveis numeroUm e stringUm não tem o mesmo valor!');
+//}
+
+let resultadoUm = numeroUm == stringUm ? 'as variáveis numeroUm e stringUm tem valores iguais, porém, possuem tipos diferentes!' : 'As variáveis numeroUm e stringUm não tem o mesmo valor!';
+console.log(resultadoUm);
+
+//  if (numeroTrinta == stringTrinta) {
+//     console.log('as variáveis numeroTrinta e stringTrinta tem valores iguais, porém, possuem tipos diferentes!');
+// } else {
+//     console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo valor!');
+// }
+
+let resultadoTrinta = numeroTrinta == stringTrinta ? 'as variáveis numeroTrinta e stringTrinta tem valores iguais, porém, possuem tipos diferentes!' : 'As variáveis numeroTrinta e stringTrinta não tem o mesmo valor!';
+console.log(resultadoTrinta);
+
+//////if (numeroDez == stringDez) {
+//    console.log('as variáveis numeroDez e stringDez tem valores iguais, porém, possuem tipos diferentes!');
+//////} else {
+//    console.log('As variáveis numeroDez e sstringDez não tem o mesmo valor!');
+//}
+
+let resultadoDez = numeroDez == stringDez ? 'as variáveis numeroDez e stringDez tem valores iguais, porém, possuem tipos diferentes!' : 'As variáveis numeroDez e stringDez não tem o mesmo valor!';
+console.log(resultadoDez);
+
